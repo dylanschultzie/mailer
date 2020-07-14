894cf72639f5d45e8b21e1f15c23aa1777a3ee4b
Dylan Schultz <dylanschultz311@gmail.com>
2020-07-14 10:16:04 -0700

    Use cookies for User authentication

--- index.js
+++ index.js
@@ -1,13 +1,26 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
 const keys = require('./config/keys');
-const app = express();
 
 require('./models/User');
 require('./services/passport');
-require('./routes/authRoutes')(app);
 
 mongoose.connect(keys.mongoURI, { useNewUrlParser: true, useUnifiedTopology: true });
+
+const app = express();
+app.use(
+  cookieSession({
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+    keys: [keys.cookieKey],
+  })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+require('./routes/authRoutes')(app);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
