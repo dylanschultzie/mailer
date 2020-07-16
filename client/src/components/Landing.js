c551ed5fa72431b098f03007b7e7afdd3fad273f
Dylan Schultz <dylanschultz311@gmail.com>
2020-07-16 13:02:24 -0700

    Flush out Landing a bit

---
+++ client/src/components/Landing.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export const Landing = () => {
+  return (
+    <div style={{ textAlign: 'center' }}>
+      <h1>Mailer!</h1>
+      <p>Collect feedback from your users!</p>
+    </div>
+  );
+};
