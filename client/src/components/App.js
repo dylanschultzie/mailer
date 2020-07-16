c763dd8371f6789a46c789628543b6ee4ef664ff
Dylan Schultz <dylanschultz311@gmail.com>
2020-07-16 11:59:15 -0700

    Update App to use hooks rather than class

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,6 +1,6 @@
-import React, { Component } from 'react';
+import React, { useEffect } from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
-import { connect } from 'react-redux';
+import { useDispatch } from 'react-redux';
 import * as actions from '../actions';
 
 import { Header } from './Header';
@@ -8,25 +8,22 @@ const Dashboard = () => <h2>Dashboard</h2>;
 const SurveyNew = () => <h2>SurveyNew</h2>;
 const Landing = () => <h2>Landing</h2>;
 
-class App extends Component {
-  componentDidMount() {
-    this.props.fetchUser();
-  }
+export const App = (props) => {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(actions.fetchUser());
+  }, []);
 
-  render() {
-    return (
-      <div className="container">
-        <BrowserRouter>
-          <div>
-            <Header />
-            <Route exact path="/" component={Landing} />
-            <Route exact path="/surveys" component={Dashboard} />
-            <Route path="/surveys/new" component={SurveyNew} />
-          </div>
-        </BrowserRouter>
-      </div>
-    );
-  }
-}
-
-export default connect(null, actions)(App);
+  return (
+    <div className="container">
+      <BrowserRouter>
+        <div>
+          <Header />
+          <Route exact path="/" component={Landing} />
+          <Route exact path="/surveys" component={Dashboard} />
+          <Route path="/surveys/new" component={SurveyNew} />
+        </div>
+      </BrowserRouter>
+    </div>
+  );
+};
