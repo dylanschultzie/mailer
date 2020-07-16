986936c7b861d7a313fc14bb3cf65dc498e22544
Dylan Schultz <dylanschultz311@gmail.com>
2020-07-16 10:08:59 -0700

    Update Header links a bit

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,6 +1,27 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 
 export const Header = () => {
+  const auth = useSelector((state) => state.auth);
+  function renderContent(auth) {
+    switch (auth) {
+      case null:
+        return;
+      case false:
+        return (
+          <li>
+            <a href="/auth/google">Sign in with Google</a>
+          </li>
+        );
+      default:
+        return (
+          <li>
+            <a>Logout</a>
+          </li>
+        );
+    }
+  }
+
   return (
     <nav>
       <div className="nav-wrapper">
@@ -8,9 +29,7 @@ export const Header = () => {
           Mailer
         </a>
         <ul id="nav-mobile" className="right hide-on-med-and-down">
-          <li>
-            <a href="sass.html">Login with Google</a>
-          </li>
+          {renderContent(auth)}
         </ul>
       </div>
     </nav>
