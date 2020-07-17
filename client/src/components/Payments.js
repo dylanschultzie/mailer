487c023d926800697f9eb5cf003214c8db898421
Dylan Schultz <dylanschultz311@gmail.com>
2020-07-17 13:58:43 -0700

    Create action for handling Payments

--- client/src/components/Payments.js
+++ client/src/components/Payments.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import StripeCheckout from 'react-stripe-checkout';
+import { useDispatch } from 'react-redux';
+import { handleToken } from '../actions';
 
 export const Payments = () => {
+  const dispatch = useDispatch();
+
   return (
     <StripeCheckout
       name="Mailer"
       description="$5 for 5 email credits"
       amount={500}
-      token={(token) => console.log(token)}
+      token={(token) => dispatch(handleToken(token))}
       stripeKey={process.env.REACT_APP_STRIPE_KEY}
     >
       <button className="btn">Add Credits</button>
